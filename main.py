14598e8e46d3ff0d4c805f15ab84405c1a4652bd
MerelFlorian <merelflorian146@gmail.com>
2022-11-20 20:10:50 +0100

    made main better

--- main.py
+++ main.py
@@ -6,20 +6,28 @@ import os
 
 if __name__ == "__main__":
 
-    HEURISTIC = "JW"
+    HEURISTIC = "NONE"
+    dir = "/home/m_rosa/SAT/SAT_solver/resources/sudokus_extra/"
 
     line = []
     start = time.time()
+    counter = 0
         
     # run algorithm for all sudoku's
-    for file in os.listdir("/home/m_rosa/SAT/SAT_solver/resources/sudokus/"):
-        sat = SAT("/home/m_rosa/SAT/SAT_solver/resources/sudokus/" + file)
+    for file in os.listdir(dir):
+        counter += 1
+        print("sudoku = ", file)
+        if counter % 50 == 0: 
+            print(counter, "ARE COMPLETED OUT OF 1011")
+        sat = SAT("/home/m_rosa/SAT/SAT_solver/resources/sudokus_extra/" + file)
         sat.input()
         dpll_sud = DPLL(sat)
         bool, count =  dpll_sud.run(sat.variables, sat.clauses, sat.set_variables, False, None, HEURISTIC, 2)
     
-        # save stats file 
+        
         line.append(str(count))
+   
+    # save stats file  
 
     with open('stats/results/stats.txt', 'w') as f:
         f.write(HEURISTIC + " ")     
