b1aecb3dea4e8234a2dffeb3c84f26afed0e273f
MerelFlorian <merelflorian146@gmail.com>
2022-11-24 15:24:14 +0100

    stats

--- stats/stats.py
+++ stats/stats.py
@@ -5,6 +5,7 @@ import pingouin as pg
 import numpy as np
 import seaborn as sns
 from scipy.stats import iqr
+import matplotlib.pyplot as plt
 # implements statsistics for KP 2022 - Artificial Intelligence
 
 # read data
@@ -13,22 +14,30 @@ d = {}
 for file in os.listdir("/home/m_rosa/SAT/SAT_solver/stats/results/"):
     file_open = open("/home/m_rosa/SAT/SAT_solver/stats/results/" + file, "r")
 
+    d["ID"] = []
+    d["amount of splits"] = []
+    d["heuristic"] = []
     # read file
     for line in file_open:
-        values = line.strip("\n").split(",")[1:]
-        d[line.split(" ")[0]] = list(map(int, values))
-    d["ID"] = list(range(1,4))
+        value = line.strip("\n").split(";")[1]
+        algorithm = line.strip("\n").split(";")[0]
+        ID = line.strip("\n").split(";")[2]
+        # d[line.split(" ")[0]] = list(map(int, value))
+        d["ID"].append(ID)
+        d["amount of splits"].append(float(value))
+        d["heuristic"].append(algorithm)
 print(d)
 
 data_panda = pd.DataFrame.from_dict(data = d)
+print(data_panda)
 
 # repeated measures with sphericity and normal distribution test
-data_panda = pd.melt(data_panda.reset_index(), id_vars= ["ID"], value_vars= ["MOM", "JW", "DPLL"])
+# data_panda = pd.melt(data_panda.reset_index(), id_vars= ["ID"], value_vars= ["MOM", "JW", "DPLL"])
 print(data_panda)
-print("RM ANOVA \n", pg.rm_anova(data = data_panda, dv = "value",within = "variable", subject = "ID", effsize = "np2", detailed = False, correction = 'auto'), "\n")
+print("RM ANOVA \n", pg.rm_anova(data = data_panda, dv = "amount of splits",within = "heuristic", subject = "ID", effsize = "np2", detailed = False, correction = 'auto'), "\n")
 
 # post hoc
-print("POST HOC \n", pg.pairwise_tests(dv = "value", within = "variable", subject = "ID", data = data_panda))
+print("POST HOC \n", pg.pairwise_tests(dv = "amount of splits", within = "heuristic", subject = "ID", data = data_panda))
 
 # plot
 # min
@@ -49,13 +58,16 @@ def max(x):
 
 sns.set()
 
-print("parametric data\n", data_panda.groupby(['variable'])['value'].agg(['mean', 'std']).round(2))
-print("non parametric data\n", data_panda.groupby(['variable'])['value'].agg(['median', iqr, q25, q75, min, max]).round(2))
+print("parametric data\n", data_panda.groupby(['heuristic'])['amount of splits'].agg(['mean', 'std']).round(2))
+print("non parametric data\n", data_panda.groupby(['heuristic'])['amount of splits'].agg(['median', iqr, q25, q75, min, max]).round(2))
 
-plot = sns.pointplot(data=data_panda, x="variable", y="value", errorbar=("pi", 100), capsize=.4, join=True, color="black", estimator = "median")
-sns.swarmplot(x="variable", y="value", data=data_panda, edgecolor="black", linewidth=.9)
-sns.boxplot(data=data_panda, x="variable", y="value", color="gray", saturation=0.05)
+plot = sns.pointplot(data=data_panda, x="heuristic", y="amount of splits", errorbar=("pi", 100), capsize=.4, join=True, color="black", estimator = "median")
+sns.swarmplot(x="heuristic", y="amount of splits", data=data_panda, edgecolor="black", linewidth=.9)
+sns.boxplot(data=data_panda, x="heuristic", y="amount of splits", color="gray", saturation=0.05)
 fig = plot.get_figure()
 fig.suptitle('Amount of splitting to find a solution')
 fig.savefig("stats2.png")
 
+df = data_panda
+plot = sns.catplot(kind = "point", data = data_panda, x="heuristic", y="amount of splits", hue = "ID", order = ["DPLL", "MOM", "JW"])
+plt.savefig('output.png')
