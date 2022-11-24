58df8666a5ff5acd28a9736b32f60ec6973dde2d
MerelFlorian <merelflorian146@gmail.com>
2022-11-24 15:14:45 +0100

    Multi.py updated

--- multi.py
+++ multi.py
@@ -16,17 +16,21 @@ def run(heuristic, file):
     sat.input()
     dpll_sud = DPLL(sat)
     bool, count =  dpll_sud.run(sat.variables, sat.clauses, sat.set_variables, False, None, heuristic)
-    print(bool, file)
-    return count, heuristic
+    return heuristic, count, file
    
 if __name__ == "__main__":
-    TOTAL = 3033
+    START = 1 # Jiseon set this to 250, Ruby set this to 500
+    TOTAL = 9
     with Pool(processes = cpu_count()) as pool:
-        result = pool.starmap(run, [("DPLL" if i <= TOTAL/3 else "MOM" if i <= TOTAL*2/3 else "JW", i if i <= TOTAL/3 else i - TOTAL//3 if i <= TOTAL*2/3 else i - (TOTAL*2)//3) for i in range(1, TOTAL+1)])
+        # result = pool.starmap(run, [("DPLL" if i <= TOTAL/3 else "MOM" if i <= TOTAL*2/3 else "JW", i if i <= TOTAL/3 else i - TOTAL//3 if i <= TOTAL*2/3 else i - (TOTAL*2)//3) for i in range(1, TOTAL+1)])
+        result = pool.starmap(run, [("DPLL" if i < START + TOTAL/3 else "MOM" if i < START + TOTAL*2/3 else "JW", i if i < START + TOTAL/3 else i - TOTAL//3 if i < START + TOTAL*2/3 else i - (TOTAL*2)//3) for i in range(START, START + TOTAL)])
+
+
+
     result = sorted(result, key=itemgetter(1))
     print(result)
     with open("stats.csv", "w") as f:
-        writer = csv.writer(f, escapechar=' ', quoting=csv.QUOTE_NONE)
-        writer.writerows([[f"{'DPLL' if i == 1 else 'JW' if i == 2 else 'MOM'},{r[0]}" if n == 0 else r[0] for n, r in enumerate(result[j:int((i*TOTAL/3))])] for i,j in zip(range(1,4), range(0, TOTAL, 1011))])
-    
+        writer = csv.writer(f, delimiter =";", escapechar =' ',quoting=csv.QUOTE_NONE)
+        # writer.writerows([[f"{'DPLL' if i == 1 else 'JW' if i == 2 else 'MOM'};{(r[0],r[2])}" if n == 0 else (r[0],r[2]) for n, r in enumerate(result[j:int((i*TOTAL/3))])] for i,j in zip(range(1,4), range(0, TOTAL, 3))])
+        writer.writerows(result)
         
